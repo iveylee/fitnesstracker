8388c8958126a313cd08c83dde2524d661627be7
ilee <ivey.lee@icloud.com>
2020-12-12 23:45:09 -0800

    revised

--- models/workout.js
+++ models/workout.js
@@ -13,16 +13,16 @@ const workoutSchema = new Schema(
         type: {
           type: String,
           trim: true,
-          required: "Enter an exercise type"
+          required: "Exercise type"
         },
         name: {
           type: String,
           trim: true,
-          required: "Enter an exercise name"
+          required: "Exercise name"
         },
         duration: {
           type: Number,
-          required: "Enter an exercise duration in minutes"
+          required: "Exercise duration in minutes"
         },
         weight: {
           type: Number
